8af9a73e98074ab2929fb3d02108949d4510dd8e
David.Marcial <davidmarcial@Davids-MacBook-Pro-2.local>
2023-10-10 20:02:21 -0600

    Removed personal vendor id and used original one

--- src/x-759224-menu-builder-uic/index.js
+++ src/x-759224-menu-builder-uic/index.js
@@ -25,7 +25,7 @@ const view = (
   return <menu-ui menuTree={menuTree}></menu-ui>;
 };
 
-createCustomElement("x-512099-menu-builder-h23", {
+createCustomElement("x-759224-menu-builder-uic", {
   renderer: { type: snabbdom },
   view,
   styles,
