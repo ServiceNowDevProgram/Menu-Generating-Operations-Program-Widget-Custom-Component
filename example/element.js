284952f448cc2469e2b214399b9d374c43933f3f
Deepak Singh <deepak.singh@intimetec.com>
2023-10-12 09:33:47 +0530

    refactor: custom-component-revert revert configuration

--- example/element.js
+++ example/element.js
@@ -4,5 +4,5 @@ const el = document.createElement('DIV');
 document.body.appendChild(el);
 
 el.innerHTML = `		
-<x-1114407-menu-builder-uic></x-1114407-menu-builder-uic>
+<x-759224-menu-builder-uic></x-759224-menu-builder-uic>
 `;
